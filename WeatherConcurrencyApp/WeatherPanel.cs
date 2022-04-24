457e819ff84ae4a754257dd3aae13a0bc7fb286e
Moises David Nunez Sediles <moises.nunez29u@std.uni.edu.ni>
2022-04-24 16:31:41 -0600

    Arreglando DetailWeather

--- WeatherConcurrencyApp/WeatherPanel.cs
+++ WeatherConcurrencyApp/WeatherPanel.cs
@@ -20,55 +20,55 @@ namespace WeatherConcurrencyApp
         private void WeatherPanel_Load(object sender, EventArgs e)
         {
             DetailsWeather dw1 = new DetailsWeather();
-            dw1.lblDetailValue.Text = "lon";
+            dw1.lblDetail.Text = "lon";
             flpContent.Controls.Add(dw1);
 
             DetailsWeather dw2 = new DetailsWeather();
-            dw2.lblDetailValue.Text = "lat";
+            dw2.lblDetail.Text = "lat";
             flpContent.Controls.Add(dw2);
 
             DetailsWeather dw3 = new DetailsWeather();
-            dw3.lblDetailValue.Text = "temp";
+            dw3.lblDetail.Text = "temp";
             flpContent.Controls.Add(dw3);
 
             DetailsWeather dw4 = new DetailsWeather();
-            dw4.lblDetailValue.Text = "feels_like";
+            dw4.lblDetail.Text = "feels_like";
             flpContent.Controls.Add(dw4);
 
             DetailsWeather dw5 = new DetailsWeather();
-            dw5.lblDetailValue.Text = "temp_min";
+            dw5.lblDetail.Text = "temp_min";
             flpContent.Controls.Add(dw5);
 
             DetailsWeather dw6 = new DetailsWeather();
-            dw6.lblDetailValue.Text = "temp_max";
+            dw6.lblDetail.Text = "temp_max";
             flpContent.Controls.Add(dw6);
 
             DetailsWeather dw7 = new DetailsWeather();
-            dw7.lblDetailValue.Text = "pressure";
+            dw7.lblDetail.Text = "pressure";
             flpContent.Controls.Add(dw7);
 
             DetailsWeather dw8 = new DetailsWeather();
-            dw8.lblDetailValue.Text = "humidity";
+            dw8.lblDetail.Text = "humidity";
             flpContent.Controls.Add(dw8);
 
             DetailsWeather dw9 = new DetailsWeather();
-            dw9.lblDetailValue.Text = "visibility";
+            dw9.lblDetail.Text = "visibility";
             flpContent.Controls.Add(dw9);
 
             DetailsWeather dw10 = new DetailsWeather();
-            dw10.lblDetailValue.Text = "speed";
+            dw10.lblDetail.Text = "speed";
             flpContent.Controls.Add(dw10);
 
             DetailsWeather dw11 = new DetailsWeather();
-            dw11.lblDetailValue.Text = "sunrise";
+            dw11.lblDetail.Text = "sunrise";
             flpContent.Controls.Add(dw11);
 
             DetailsWeather dw12 = new DetailsWeather();
-            dw12.lblDetailValue.Text = "sunset";
+            dw12.lblDetail.Text = "sunset";
             flpContent.Controls.Add(dw12);
 
             DetailsWeather dw13 = new DetailsWeather();
-            dw13.lblDetailValue.Text = "timezone";
+            dw13.lblDetail.Text = "timezone";
             flpContent.Controls.Add(dw13);
         }
     }
